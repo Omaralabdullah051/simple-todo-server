f4d41b5deb424e9249bfbba0f5cc8a46ba939894
omaralabdullah051 <omaralabdullah051@gmail.com>
2022-05-18 23:48:40 +0400

    created a post API to insert todo

--- index.js
+++ index.js
@@ -3,16 +3,37 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri = `mongodb+srv://${process.env.DB_USERS}:${process.env.DB_PASS}@cluster0.ex3wp.mongodb.net/?retryWrites=true&w=majority`;
-
-app.use(cors());
-app.use(express.json());
+require("dotenv").config();
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ex3wp.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
+
+app.use(cors());
+app.use(express.json());
+
+const run = async () => {
+  try {
+    await client.connect();
+    const todoCollection = client.db("todo_management").collection("todos");
+
+    app.post("/addtask", async (req, res) => {
+      try {
+        const todo = req.body;
+        const result = await todoCollection.insertOne(todo);
+        res.send({ success: true, result });
+      } catch (err) {
+        console.log(err);
+      }
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+run();
 
 app.get("/", (req, res) => {
   res.send("Hello!users");
